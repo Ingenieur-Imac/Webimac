f43be456f7721f9f40c5d036cf10936b52be35f8
JulienRst <julien.rousset01@gmail.com>
2016-03-03 13:04:09 +0000

    ADD STUDENT IN BACKEND (CREATE / MODIFY / DELETE)

---
+++ resources/views/admin/student/_form.blade.php
@@ -0,0 +1,37 @@
+<!-- name form input -->
+<div class="form-group">
+    {!! Form::label('name','Nom') !!}
+    {!! Form::text('name',null,['class' => 'form-control']) !!}
+</div>
+<!-- promo form input -->
+<div class="form-group">
+    {!! Form::label('promo','Promotion') !!}
+    {!! Form::select('promo',$promos,null,['class' => 'form-control']) !!}
+</div>
+<!-- url_web form input -->
+<div class="form-group">
+    {!! Form::label('url_web','Lien vers le site web') !!}
+    {!! Form::text('url_web',null,['class' => 'form-control']) !!}
+</div>
+<!-- url_linkedin form input -->
+<div class="form-group">
+    {!! Form::label('url_linkedin','Lien vers Linkedin') !!}
+    {!! Form::text('url_linkedin',null,['class' => 'form-control']) !!}
+</div>
+<!-- url_image form input -->
+@if (isset($old_url_image))
+    <div class="form-group">
+        {!! Form::label('url_image','URL de la photo de l\'étudiant | Fichier Actuel : '.$old_url_image) !!}
+        {!! Form::file('url_image',['class' => 'form-control']) !!}
+    </div>
+@else
+    <div class="form-group">
+        {!! Form::label('url_image','URL de la photo de l\'étudiant') !!}
+        {!! Form::file('url_image',['class' => 'form-control']) !!}
+    </div>
+@endif
+
+<!-- submit form input -->
+<div class="form-group">
+    {!! Form::submit($submitButtonText,['class' => 'btn btn-primary form-control']) !!}
+</div>
