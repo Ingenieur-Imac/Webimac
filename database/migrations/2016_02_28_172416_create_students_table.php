e5438fd3407f115eb5c253f5c7fc2b33285d9c47
JulienRst <julien.rousset01@gmail.com>
2016-04-27 23:54:03 +0000

    BACK-END :: Change Name -> Name + Surname on Student and Staff

--- database/migrations/2016_02_28_172416_create_students_table.php
+++ database/migrations/2016_02_28_172416_create_students_table.php
@@ -15,6 +15,7 @@ class CreateStudentsTable extends Migration
         Schema::create('students',function(Blueprint $table){
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
             $table->integer('promo')->unsigned();
             $table->string('url_image');
             $table->string('url_web');
