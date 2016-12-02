2547004310464ec99c9af2d262dfd57d0dee98d4
Julien Rousset <julien.rousset01@gmail.com>
2016-12-02 19:20:20 +0100

    [Add timer]

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -14,6 +14,7 @@
         <nav>
             @include('layouts.nav')
         </nav>
+        @include('timer')
         <div>
             @yield('content')
         </div>
--- resources/views/timer.blade.php
+++ resources/views/timer.blade.php
@@ -1,6 +1,3 @@
-@extends('app')
-
-@section('content')
-    <script> var dateJpo = {{$ts}}</script>
-    <div id="timer"></div>
-@endsection
+<div id="timer">
+    Journées Portes Ouvertes : 25 Février 2017 - de 9h30 à 17h
+</div>
